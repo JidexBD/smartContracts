895c1fa66b7c243600b6aa6f19464a9d47862177
JidexBD <ewuolajide7@gmail.com>
2025-05-15 23:44:49 +0100

    storage factory somehow

--- Contracts/StorageFactory.sol
+++ Contracts/StorageFactory.sol
@@ -1,8 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-contract StorageFactory { 
+//imports from our simple storage contract
+import {SimpleStorage} from "./SimpleStorage.sol";
 
+
+
+contract StorageFactory { 
+  
+    //stores our simple storage contracts
+    SimpleStorage [] public listOfSimpleStorageContracts;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage newSimpleStorageContract = new   SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
+    }
+
+
+    //his function is used for storing data into your list of Simple Storage smart contracts based on index and number.
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        mySimpleStorage.store(_newSimpleStorageNumber);
+    }
+
+    function sfget(uint256 _simpleStorageIndex)public view returns(uint256){
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
     
+    }
+
 
 }
