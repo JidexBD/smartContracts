5415fe80949e84d051bbda0e04741c680f366bc5
JidexBD <ewuolajide7@gmail.com>
2025-05-16 23:04:25 +0100

    fund me and twitter

--- Contracts/FundMe.sol
+++ Contracts/FundMe.sol
@@ -1,2 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
+
+contract FundMe {
+    function fund() public payable {
+        
+    }
+}
--- Contracts/Twitter.sol
+++ Contracts/Twitter.sol
@@ -1,2 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
+
+
+
+contract Twitter {
+    
+    mapping (address => string) public tweets;
+
+    function createTweet(string memory _tweet) public {
+        tweets[msg.sender] = _tweet;
+    }
+}
